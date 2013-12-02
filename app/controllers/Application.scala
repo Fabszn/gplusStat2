1ad4dc0b214a7a60e0bbb0a5171b61e186bdb220
Fabrice Sznajderman <fabszn@gmail.com>
2013-12-02 20:27:53 +0100

    datatables version 1 : up

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,20 +3,32 @@ package controllers
 import play.api._
 import play.api.mvc._
 import utils.{GooglePlusAPIHelper}
-import reactivemongo.bson.{BSONDocumentWriter, BSONString, BSONDocument, BSONDocumentReader}
+import reactivemongo.bson._
 import model.Article
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import reactivemongo.api.collections.default.BSONCollection
 import play.api.Play.current
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
+import play.api.libs.json._
+import reactivemongo.bson.BSONString
+import reactivemongo.api.collections.default.BSONCollection
+import scala.util.parsing.json.JSONArray
+import reactivemongo.bson.BSONLong
+import play.api.libs.json.JsArray
+import reactivemongo.bson.BSONBoolean
+import reactivemongo.bson.BSONString
+import reactivemongo.api.collections.default.BSONCollection
+
 
 object Application extends Controller {
 
   def index = Action {
+    Ok(views.html.index(""))
+  }
 
-
-    Ok(views.html.index("" + GooglePlusAPIHelper.getArticles().size))
+  def index2 = Action {
+    Ok(Json.toJson(List[String]("Hello World", "toto")))
   }
 
 
@@ -25,7 +37,12 @@ object Application extends Controller {
       //al doc = document.toTraversable
       Article(
         document.getAs[BSONString]("title").get.value,
-        document.getAs[BSONString]("googleId").get.value)
+        document.getAs[BSONString]("googleId").get.value,
+        document.getAs[BSONString]("content").get.value,
+        document.getAs[BSONString]("author").get.value,
+        document.getAs[BSONLong]("plusOne").get.value,
+        document.getAs[BSONLong]("shared").get.value,
+        document.getAs[BSONBoolean]("current").get.value)
     }
   }
 
@@ -34,7 +51,12 @@ object Application extends Controller {
       BSONDocument(
         //"_id" -> article.id.getOrElse(BSONObjectID.generate),
         "title" -> BSONString(article.title),
-        "googleId" -> BSONString(article.googleId)
+        "googleId" -> BSONString(article.googleId),
+        "content" -> BSONString(article.content),
+        "author" -> BSONString(article.author),
+        "plusone" -> BSONLong(article.plusone),
+        "shared" -> BSONLong(article.shared),
+        "current" -> BSONBoolean(article.current)
       )
 
     /*if (article.creationDate.isDefined)
@@ -45,8 +67,20 @@ object Application extends Controller {
 
   }
 
+
+
   def displayArticles = Action.async {
 
+    implicit object articleJsonWrite extends Writes[Article] {
+      def writes(o: Article): JsValue = JsArray(Seq(JsString(o.title),
+        //"googleId" -> o.googleId,
+        //"content" -> o.content,
+        JsString(o.author),
+        JsString(""+o.plusone),
+        JsString(""+o.shared)
+        //"current" -> o.current
+      ))
+    }
 
     // Gets a reference to the collection "acoll"
     // By default, you get a BSONCollection.
@@ -55,10 +89,9 @@ object Application extends Controller {
 
     val cursor = collection.find(query).cursor[Article]
 
-
     val futureList = cursor.collect[List](100)
-
-    futureList.map(articles => Ok(views.html.allArticles(articles.map(a => a.toString))))
+    //OK(Json.toJson(Article("","","","",12l,12l,true)))
+    futureList.map(articles => Ok(JsObject(Seq(("aaData",Json.toJson(articles))))))
   }
 
 }
--- app/model/Article.scala
+++ app/model/Article.scala
@@ -1,7 +1,23 @@
 package model
 
+import play.api.libs.json.{JsValue, Writes}
+import play.api.libs.json._
 
-case class Article(title: String, googleId: String)
+
+case class Article(title: String, googleId: String, content: String, author: String, plusone: Long, shared: Long, current: Boolean)
+
+/*
+* private String title;
+    private String googleId;
+    private String content;
+    private String author;
+    private long publicationDate;
+    private long insertionDate;
+    private long plusOne;
+    private long shared;
+    private boolean current = false;
+    private String url;
+* */
 
 
 object Article {
